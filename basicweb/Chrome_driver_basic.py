a6ac4d5576697412f9f7ef5cbf6413f62136cd30
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-04 22:05:18 +0530

    added basic IE driver program and somedocstring to existing programs

--- basicweb/Chrome_driver_basic.py
+++ basicweb/Chrome_driver_basic.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 
 class ChromeTest():
+
+# basic function to get the google from chrome driver
     def basicrun(self):
+        # Provide the chrome driver path
         driver = webdriver.Chrome(executable_path="D:\study\python\Ch_driver\chromedriver.exe")
+        # Launch the google site
         driver.get("https://www.google.com")
 
 ch_Obj1 = ChromeTest()
--- basicweb/Firefox_driver_basic.py
+++ basicweb/Firefox_driver_basic.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 
 class FirefoxTest():
+
+    # basic function to get the google from Firefox driver
     def basicrun(self):
+        # Provide the firefox driver path
         driver = webdriver.Firefox(executable_path="D:\study\python\FF_driver\geckodriver.exe")
+        # Launch the google site
         driver.get("https://www.google.com")
 
 Ff_obj1 = FirefoxTest()
