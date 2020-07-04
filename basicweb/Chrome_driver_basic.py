c755b3e43512d63c22da9b6fb7eac4700871504b
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-04 22:53:13 +0530

    all kind of driver path possibilities with IE, chrome, firefox

--- basicweb/Chrome_driver_basic.py
+++ basicweb/Chrome_driver_basic.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+import os
 
 class ChromeTest():
 
 # basic function to get the google from chrome driver
     def basicrun(self):
         # Provide the chrome driver path
-        driver = webdriver.Chrome(executable_path="D:\study\python\Ch_driver\chromedriver.exe")
+        # driver = webdriver.Chrome(executable_path="D:\study\python\Drivers\chromedriver.exe")
+        # if this programs runs to different OS platform, then Above will not worked. check below
+        #driverlocation = "D:\study\python\Drivers\chromedriver.exe"
+        #os.environ("Webdriver chrome location") = driverlocation
+        driver = webdriver.Chrome(driverlocation)
+        # Without The Driver path set ( For Linux all drivers put together into /usr/local/bin directory, In Windows add the system path in enviroinment variables )
+        driver = webdriver.Chrome()
         # Launch the google site
         driver.get("https://www.google.com")
 
--- basicweb/Firefox_driver_basic.py
+++ basicweb/Firefox_driver_basic.py
@@ -5,7 +5,9 @@ class FirefoxTest():
     # basic function to get the google from Firefox driver
     def basicrun(self):
         # Provide the firefox driver path
-        driver = webdriver.Firefox(executable_path="D:\study\python\FF_driver\geckodriver.exe")
+        # driver = webdriver.Firefox(executable_path="D:\study\python\Drivers\geckodriver.exe")
+        # Without The Driver path set ( For Linux all drivers put together into /usr/local/bin directory, In Windows add the system path in enviroinment variables )
+        driver = webdriver.Firefox()
         # Launch the google site
         driver.get("https://www.google.com")
 
