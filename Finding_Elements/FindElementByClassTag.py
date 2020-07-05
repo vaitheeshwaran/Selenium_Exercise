12af95a5a71e8b664af7613f44b268dea8872d1c
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-05 18:28:27 +0530

    Added class and tag element program file with doc string

--- Finding_Elements/FindElementByClassTag.py
+++ Finding_Elements/FindElementByClassTag.py
@@ -8,8 +8,9 @@ class Findbyclasstag():
         ElementByclass = driver.find_element_by_class_name("inputs")
         if ElementByclass is not None:
             print("Found an element class")
-
+        # for below one it will take all the anchors tag element
         #ElementBytag = driver.find_element_by_tag_name("a")
+        # below there is only one h1 tag, so there is no issue, mostly tag element will not be used
         ElementBytag = driver.find_element_by_tag_name("h1")
         text = ElementBytag.text
         if ElementBytag is not None:
