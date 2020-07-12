08d5f9f223dad39211015d60cae26336f79261e6
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-12 22:22:24 +0530

    Modified with clicck  all courses and search with different cource

--- Workingwithelements/dynamic_xpath_format.py
+++ Workingwithelements/dynamic_xpath_format.py
@@ -8,7 +8,9 @@ class DynamicXPathFormat():
 
     def test(self):
         baseUrl = "https://letskodeit.teachable.com"
-        driver = webdriver.Firefox()
+        opt = webdriver.ChromeOptions()
+        opt.add_argument("user-data-dir=C:\\Users\\vaitheeshwaran.v\\AppData\Local\\Google\\Chrome\\User Data\\Default")
+        driver = webdriver.Chrome(options=opt)
         driver.maximize_window()
         driver.implicitly_wait(10)
         driver.get(baseUrl)
@@ -21,17 +23,22 @@ class DynamicXPathFormat():
         password.send_keys("abcabc")
         driver.find_element(By.NAME, "commit").click()
 
+        # Click on all courses text
+        MyCourse = driver.find_element_by_partial_link_text("All Courses")
+        MyCourse.click()
+
         # Search for courses -> You don't need to search the course
         # You can select it without searching also
         searchBox = driver.find_element(By.ID, "search-courses")
-        searchBox.send_keys("JavaScript")
+        searchBox.send_keys("Selenium")
 
         # Select Course
         _course = "//div[contains(@class,'course-listing-title') and contains(text(),'{0}')]"
-        _courseLocator = _course.format("JavaScript for beginners")
+        _courseLocator = _course.format("Selenium WebDriver With Java")
 
         courseElement = driver.find_element(By.XPATH, _courseLocator)
         courseElement.click()
+        time.sleep(5)
 
 
 ff = DynamicXPathFormat()
