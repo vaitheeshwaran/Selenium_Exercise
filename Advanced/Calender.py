72ecc0778cb799913541d3b7da7d417229ea8ee4
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-15 15:13:18 +0530

    Added Another calender function with take elements list in loop and find the text using some text condition

--- Advanced/Calender.py
+++ Advanced/Calender.py
@@ -39,6 +39,29 @@ class Calender_Selection():
 
         driver.quit()
 
+    def test2(self):
+        baseUrl = "http://www.expedia.com"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(3)
+
+        # Click flights tab
+        driver.find_element_by_id("tab-flight-tab").click()
+        # Click departing field
+        driver.find_element_by_id("flight-departing").click()
+        # Expedia website has changed the DOM after the lecture was made
+        # Updated new xpath
+        calMonth = driver.find_element(By.XPATH, "(//div[@class='datepicker-cal-month'])[1]")
+        allValidDates = calMonth.find_elements(By.TAG_NAME, "button")
+
+        time.sleep(2)
+
+        for date in allValidDates:
+            if date.text == "30":
+                date.click()
+                break
+
 
 Calender_obj1 = Calender_Selection()
 Calender_obj1.selection_date()
