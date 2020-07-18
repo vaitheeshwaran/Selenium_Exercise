eb86a7cf30d59a52c45412d7884a2d9af2f43b5e
vaitheeshwaran.v <vaitheeshhh@gmail.com>
2020-07-18 11:58:21 +0530

    Added pytest module with yield keyword . [ it will used to execute before and after the execution of every method ]

---
+++ PytestModules/test_case_demo1.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.fixture()
+def setUp():
+    print("Running demo1 setUp")
+    yield
+    print("Running After execution")
+
+def test_demo1_methodA(setUp):
+    print("Running demo1 method A")
+
+def test_demo1_methodB(setUp):
+    print("Running demo1 method B")
